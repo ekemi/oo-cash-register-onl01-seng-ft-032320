a6eeb512631deda6fd2cd9776721e3b77f55f754
edmundo papi <edmundopierre@yahoo.com>
2020-04-09 17:27:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,39 @@
+require 'pry'
 class CashRegister
+  @@all =[]
+ attr_accessor :discount ,:employee_discount,:total, :quantity, :title, :items
 
-  def initialize(employee_discount)
+  def initialize(discount = 0)
     @employee_discount = employee_discount
     @total = 0
+    @discount =discount
+    @quantity = quantity
+    @title = title
   end
 
- def total
+  def total
+    @total
+   end
+
+   def add_item( title,price, quantity = 1)
+
+     @total =(price * quantity) + @total
+    #binding.pry
 end
+
+def apply_discount
+  # binding.pry
+  if @discount != 0
+    @total = @total - (@total * @discount/100)
+    return "After the discount, the total comes to $#{@total}."
+  #  binding.pry
+  else
+    return "There is no discount to apply."
+  end
+end
+  def items
+ @items
+
+  end
+end
+#
